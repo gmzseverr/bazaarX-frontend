9bf3cb7cf2ad1e1fab93d32ddeaaaa011fbac5bc
gmzseverr <gmzsever12@gmail.com>
2025-07-11 10:24:50 +0300

    fix shop page

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -1,13 +1,17 @@
-// src/app/shop/page.jsx
-"use client"; // ProductList Client Component ise bu da client olmalı
+"use client";
 
 import ProductList from "@/app/components/ProductList";
+import { Suspense } from "react";
 
 export default function ShopPage() {
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-3xl font-bold text-center mb-8">Tüm Ürünler</h1>
-      <ProductList />
+      <h1 className="text-3xl font-bold text-center mb-8">All Products</h1>
+      <Suspense
+        fallback={<div className="text-center">Loading products...</div>}
+      >
+        <ProductList />
+      </Suspense>
     </div>
   );
 }
